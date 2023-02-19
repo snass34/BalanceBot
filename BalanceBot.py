303902754e861c7c2f3bb9d17bcb04295aa93e9b
Jessica Duong <54159494+jessduong@users.noreply.github.com>
2023-02-18 19:51:42 -0800

    Basic ChatBot code

--- BalanceBot.py
+++ BalanceBot.py
@@ -1,3 +1,20 @@
-#include chatterbot
+# pip install chatterbot==1.0.4
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
-print('CHAT CHAT')
+chatbot = ChatBot('BalanceBot')
+trainer = ListTrainer(chatbot)
+l = [["Hi", "Welcome, friend 🤗",],
+     ["Hi", "bananas, friend",],
+     ["Are you a plant?", "No, I'm the pot below the plant!"]]
+
+for item in l:
+    trainer.train(item)
+
+exit_conditions = (":q", "quit", "exit")
+while True:
+    query = input("> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"🪴 {chatbot.get_response(query)}")
